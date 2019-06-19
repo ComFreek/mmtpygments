4b30c74e36888635d4b9413f4575a83b19c2aafd
ComFreek <comfreek@outlook.com>
2019-06-19 11:15:37 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# MMT Pygments Lexer
+
+![Screenshot of highlighted MMT code](https://user-images.githubusercontent.com/1827709/59698193-7523c300-91ef-11e9-8c4b-80ec2d3e4a40.png)
+
+# Usage
+
+We don't have a Pip package currently, hence we recommend cloning this repository as a Git submodule:
+
+```bash
+git submodule add mmt-pygments-lexer https://github.com/ComFreek/mmt-pygments-lexer.git
+```
+
+## Usage in LaTeX
+
+```tex
+% You MUST USE XeLaTeX (or other LaTeX-derivatives which support Unicode)
+\usepackage{fontspec}
+
+% Download GNU Unifont from http://unifoundry.com/unifont/index.html
+% And save it, say, as "fonts/unifont-12.1.02.ttf"
+\newfontfamily\unifont{unifont-12.1.02.ttf}[Path=./fonts/,NFSSFamily=unifont]
+
+% Disable caching for debugging purposes (increases compilation times!)
+\usepackage[cache=false]{minted}
+\setminted{
+	fontfamily=unifont
+}
+
+\begin{document}
+	% Load from a file
+	\inputminted{mmt-pygments-lexer.py:MMTLexer -x}{your-mmt-file.mmt}
+	
+	% Or inline
+	\begin{minted}{mmt-pygments-lexer.py:MMTLexer -x}
+theory MyTheory =
+	c : type ❘ # abc ❙
+❚
+\end{minted}
+\end{document}
+```
+
+See the [minted manual](https://ctan.org/pkg/minted) on how to define shortcuts lest you have to type `mmt-pygments-lexer.py:MMTLexer -x` every time.
---
+++ mmt-pygments-lexer.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+	Pygments Lexer for MMT Surface Syntax
+	~~~~~~~~~~~~~~~~~~~~
+	
+	The MMT project can be found at https://uniformal.github.io/.
+
+	:author: ComFreek <comfreek@outlook.com>
+	:copyright: Copyright 2019 ComFreek
+	:license: ISC, see LICENSE for details.
+"""
+
+import re
+
+from pygments.lexer import RegexLexer, include, bygroups, default, using, \
+	this, words, combined
+from pygments.token import Text, Comment, Operator, Keyword, Name, String, \
+	Number, Punctuation, Other, Whitespace
+from pygments.util import get_bool_opt, iteritems
+import pygments.unistring as uni
+
+__all__ = ['MMTLexer']
+
+
+class MMTLexer(RegexLexer):
+	"""
+	Pygments Lexer for MMT Surface Syntax (.mmt)
+	
+	The MMT project can be found at https://uniformal.github.io/.
+	"""
+
+	name = 'MMT'
+	aliases = ['mmt']
+	filenames = ['*.mmt']
+	mimetypes = ['text/plain']
+
+	flags = re.DOTALL | re.UNICODE | re.IGNORECASE | re.MULTILINE
+
+	tokens = {
+		'root': [
+			(r'\s', Whitespace),
+			(r'(namespace)(\s+)(\S+)(❚)', bygroups(Keyword.Namespace, Whitespace, String, Punctuation)),
+			(r'(import)(\s+)(\S+)(\s+)(\S+)(❚)', bygroups(Keyword.Namespace, Whitespace, Name.Namespace, Whitespace, String, Punctuation)),
+			(r'theory', Keyword.Declaration, 'theoryHeader'),
+			(r'view\b', Keyword.Declaration, 'viewHeader'),
+			(r'\/T .*?❚', Comment.Multiline),
+			(r'\/\/.*?❚', Comment.Multiline)
+		],
+		'theoryHeader': [
+			(r'\s', Whitespace),
+			# First try matching with meta theory
+			(r'(\S+)(\s*)(:)(\s*)(\S+)(\s*)(=)', bygroups(Name.Variable, Whitespace, Punctuation, Whitespace, Name.Variable, Whitespace, Punctuation), 'moduleBody'),
+			
+			# Then without meta theory
+			(r'(\S+)(\s*)(=)', bygroups(Name.Variable, Whitespace, Punctuation), 'moduleBody')
+		],
+		'viewHeader': [
+			(r'\s', Whitespace),
+			(r'(\S+)(\s*)(:)(\s*)(\S+)(\s*)(->)(\s*)(\S+)(\s*)(=)', bygroups(
+					Name.Variable,
+					Whitespace,
+					Punctuation,
+					Whitespace,
+					Name.Variable,
+					Whitespace,
+					Punctuation,
+					Whitespace,
+					Name.Variable,
+					Whitespace,
+					Punctuation
+			), 'moduleBody')
+		],
+		
+		# Modules subsume both theories and views
+		# Invariant: moduleBody jumps at end two levels up since it assumes a theoryHeader or viewHeader before
+		'moduleBody': [
+		  (r'\s', Whitespace),
+			(r'\/T .*?❙', Comment.Multiline),
+			(r'\/\/.*?❙', Comment.Multiline),
+			(r'include\b', Keyword.Namespace, 'includeDeclaration'),
+			(r'rule\b', Keyword.Namespace, 'ruleDeclaration'),
+			(r'[^\s:❘❙❚]+', Name.Constant, 'constantDeclaration'),
+			(r'❚', Punctuation, '#pop:2') # Jump two levels above
+		],
+		'includeDeclaration': [
+			(r'❙', Punctuation, '#pop'),
+			
+			# If not end delimiter, interpret everything else as an expression
+			(r'', Whitespace, 'expression')
+		],
+		'ruleDeclaration': [
+			(r'❙', Punctuation, '#pop'),
+			
+			# If not end delimiter, interpret everything else as an expression
+			(r'', Whitespace, 'expression')
+		],
+		'constantDeclaration': [
+			(r'\s', Whitespace),
+			(r':', Punctuation, 'expression'),
+			(r'=', Punctuation, 'expression'),
+			(r'#', Punctuation, 'notationExpression'),
+			(r'role\b', Keyword, 'expression'),
+			(r'❘', Punctuation),
+			(r'❙', Punctuation, '#pop')
+		],
+		'notationExpression': [
+			# Lexing rule for notations specifying precedence
+			(r'([^❘❙❚=]+)(\bprec)(\s+)(-?\d+)', bygroups(String, Keyword, Whitespace, Number.Integer), '#pop'),
+			
+			# And for notations without
+			# (Theoretically, both could be merged into one regex, however I couldn't figure out bygroups usage in that case.)
+			(r'([^❘❙❚=]+)', bygroups(String), '#pop')
+		],
+		'expression': [
+			(r'\s', Whitespace),
+			(r'[^❘❙❚=]+', String, '#pop')
+		],
+	}
+
+# Use this for debugging
+if __name__ == "__main__":
+	import io
+	import sys
+
+	if len(sys.argv) != 2:
+		print("Debugging Interface for MMT's Pygments Lexer")
+		print("Usage: " + sys.argv[0] + " filename-of-MMT-file")
+		print("")
+		print("It will be read in UTF-8 encoding and lexed through our parser.")
+		sys.exit()
+
+	test_file = io.open(sys.argv[1], mode="r", encoding="utf-8")
+
+	lexer = MMTLexer()
+	print(list(lexer.get_tokens(test_file.read())))
