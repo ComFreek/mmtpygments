00aa6d59eeec471dc34b8aaa425164768687fa6a
ComFreek <comfreek@outlook.com>
2020-05-20 12:22:32 +0200

    fix Travis CI not picking up the tagged commit
    See https://stackoverflow.com/questions/30156581/travis-ci-skipping-deployment-although-commit-is-tagged

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'r', newline='', encoding='utf-8') as readme_file:
 
 setup(
 	name = 'mmtpygments',
-	version = '0.3.1',
+	version = '0.3.2',
 
 	# This description field must not contain any newline characters
 	# Otherwise, the all too common "`long_description_content_type` missing"
